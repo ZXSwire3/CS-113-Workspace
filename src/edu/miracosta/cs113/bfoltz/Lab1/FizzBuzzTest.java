8d5303bb0dcf016806fa35be23c20b1cbd6c85f1
BuildTools <unconfigured@null.spigotmc.org>
2021-01-29 14:00:39 -0800

    Initial commit

---
+++ src/edu/miracosta/cs113/bfoltz/Lab1/FizzBuzzTest.java
@@ -0,0 +1,22 @@
+package edu.miracosta.cs113.bfoltz.Lab1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+
+    }
+
+}
