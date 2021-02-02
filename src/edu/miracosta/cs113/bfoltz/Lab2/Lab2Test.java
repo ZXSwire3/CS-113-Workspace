5314c577c0a1a6d900ffdef384fb7d37722b1d52
BuildTools <unconfigured@null.spigotmc.org>
2021-02-01 21:10:00 -0800

    Lab 2 JUnit test updated

--- src/edu/miracosta/cs113/bfoltz/Lab2/Lab2Test.java
+++ src/edu/miracosta/cs113/bfoltz/Lab2/Lab2Test.java
@@ -6,8 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Lab2Test {
 
+    int[] numberArray = {5, 16, 64, 80};
+    boolean[] results = {false, true, true, false};
     @Test
     void isPowerOf2() {
-        assertTrue(Lab2.isPowerOf2(4));
+        assertEquals(results[0], Lab2.isPowerOf2(numberArray[0]));
+        assertEquals(results[1], Lab2.isPowerOf2(numberArray[1]));
+        assertEquals(results[2], Lab2.isPowerOf2(numberArray[2]));
+        assertEquals(results[3], Lab2.isPowerOf2(numberArray[3]));
     }
 }
