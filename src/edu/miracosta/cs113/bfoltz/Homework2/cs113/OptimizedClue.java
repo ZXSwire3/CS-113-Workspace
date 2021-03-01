21047cb27cbe6b926b01b8563bdd56ae5591ba76
Ben Foltz <eggie.foltz@gmail.com>
2021-02-28 20:03:59 -0800

    Homework 2 added

---
+++ src/edu/miracosta/cs113/bfoltz/Homework2/cs113/OptimizedClue.java
@@ -0,0 +1,77 @@
+package edu.miracosta.cs113.bfoltz.Homework2.cs113;
+
+import edu.miracosta.cs113.bfoltz.Homework2.model.AssistantJack;
+import edu.miracosta.cs113.bfoltz.Homework2.model.Theory;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class OptimizedClue {
+    /*
+     * ALGORITHM:
+     *
+     * PROMPT "Which theory to test? (1, 2, 3[random]): "
+     * READ answerSet
+     * INSTANTIATE jack = new AssistantJack(answerSet)
+     * DO
+     *      weapon = random int between 1 and 6
+     *      location = random int between 1 and 10
+     *      murder = random int between 1 and 6
+     *      solution = jack.checkAnswer(weapon, location, murder)
+     * WHILE solution != 0
+     *
+     * OUTPUT "Total checks = " + jack.getTimesAsked()
+     * IF jack.getTimesAsked() is greater than 20 THEN
+     *      OUTPUT "FAILED"
+     * ELSE
+     *      OUTPUT "PASSED"
+     * END IF
+     */
+
+    /**
+     * Driver method for random guessing approach
+     *
+     * @param args not used for driver
+     */
+    public static void main(String[] args) {
+        // DECLARATION + INITIALIZATION
+        int answerSet, solution, murder=6, weapon=6, location=10;
+        Theory answer;
+        AssistantJack jack;
+        Scanner keyboard = new Scanner(System.in);
+        Random random = new Random();
+
+        // INPUT
+        System.out.print("Which theory would like you like to test? (1, 2, 3[random]): ");
+        answerSet = keyboard.nextInt();
+        keyboard.close();
+
+        // PROCESSING
+        jack = new AssistantJack(answerSet);
+
+        do {
+            solution = jack.checkAnswer(weapon, location, murder);
+
+
+            if (solution == 1) { //checks to see if there is an incorrect weapon
+                weapon--;
+            } else if (solution == 2) { //checks to see if there is an incorrect location
+                location--;
+            } else { //checks to see if there is an incorrect murderer
+                murder--;
+            }
+        } while (solution != 0); //loop ends when all the answers are correct
+
+        answer = new Theory(weapon, location, murder);
+
+        // OUTPUT
+        System.out.println("Total Checks = " + jack.getTimesAsked() + ", Solution " + answer);
+
+        if (jack.getTimesAsked() > 20) {
+            System.out.println("FAILED!! You're a horrible Detective...");
+        } else {
+            System.out.println("WOW! You might as well be called Batman!");
+        }
+
+    }
+}
